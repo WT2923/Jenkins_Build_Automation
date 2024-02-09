794a91f497635a9b80a5d52d0fc3b3d373f956b6
PF48Y76P\User <sahil.gevariya@wissen.com>
2024-02-09 15:13:42 +0530

    Github webhook added

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,6 +1,7 @@
 
 public class HelloJenkins {
 	public static void main(String[] args) {
-		System.out.println("Hello Jenkins!!");
+		System.err.println("******* Hello Jenkins!! *******");
+		System.err.println("Git Hub Web-Hook is added!");
 	}
 }
